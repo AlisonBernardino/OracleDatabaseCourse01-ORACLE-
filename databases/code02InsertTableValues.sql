653885d8a5c46ef3a5eacd493e42979fb89027da
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-06-12 05:30:44 -0300

    code02 changes && code05 added

--- databases/code02InsertTableValues.sql
+++ databases/code02InsertTableValues.sql
@@ -6,28 +6,44 @@ insert into vehicles(
     vehCode, vehModel, mark) 
 values(01,'Ghibli','Maserati');
 
--- My custom examples
+-- My custom examples (01/10)
 insert into vehicles( 
     vehCode, vehModel, mark) 
 values(02,'Levante','Maserati');
 
-insert into vehicles(vehCode, vehModel, mark)
-values(03,'Quattroporte','Maserati');
+-- (02/10)
+insert into vehicles(
+    vehCode, vehModel, mark)
+values(03,'458 Italia','Ferrari');
 
+-- (03/10)
 insert into vehicles(vehCode, vehModel, mark)
-values(04,'MS20','Maserati');
+values(04,'Quattroporte','Maserati');
 
+-- (04/10)
+insert into vehicles(vehCode, vehModel, mark)
+values(05,'MS20','Maserati');
+
+-- (05/10)
 insert into vehicles(vehCode, vehModel, mark)
 values(06,'Maranello','Ferrari');
 
+-- (06/10)
 insert into vehicles(vehCode, vehModel, mark)
 values(07,'Dino','Ferrari');
 
+-- (07/10)
 insert into vehicles(vehCode, vehModel, mark)
 values(08,'320i','BMW');
 
+-- (08/10)
 insert into vehicles(vehCode, vehModel, mark)
 values(09,'Z4','BMW');
 
+-- (09/10)
 insert into vehicles(vehCode, vehModel, mark)
 values(10,'E46','BMW');
+
+-- (10/10)
+insert into vehicles(vehCode, vehModel, mark)
+values(11, 'R-34 Skyline', 'Nissan');
---
+++ databases/code05WhereRestriction.sql
@@ -0,0 +1,4 @@
+-- Oracle course - Select Tech
+-- Code 05 - "Where" restriction command
+
+-- Continue from here
